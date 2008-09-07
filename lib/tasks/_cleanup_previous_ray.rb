82e226f97cf143ab43fadb374987f5ac2ac16c2f
johnmuhl <git@johnmuhl.com>
2008-09-06 18:25:33 -0600

    clean up server preferences

--- lib/tasks/_cleanup_previous_ray.rb
+++ lib/tasks/_cleanup_previous_ray.rb
@@ -2,3 +2,7 @@ check = File.new("vendor/extensions/ray/config/download.txt", "r") rescue nil
 if check != nil
   system "rm vendor/extensions/ray/config/download.txt"
 end
+check = File.new("vendor/extensions/ray/config/restart.txt", "r") rescue nil
+if check != nil
+  system "rm vendor/extensions/ray/config/restart.txt"
+end
