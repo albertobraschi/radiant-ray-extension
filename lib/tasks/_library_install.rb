cbed31d5d8945effcd37d3a291870a800c9a30a0
johnmuhl <git@johnmuhl.com>
2008-10-03 15:03:57 -0600

    cleanup output

--- lib/tasks/_library_install.rb
+++ lib/tasks/_library_install.rb
@@ -1,4 +1,3 @@
 puts "I'm about to install the #{@lib} gem."
 puts "You'll need to enter your system administrator password."
 system "sudo gem install #{@lib}"
-puts "=============================================================================="
