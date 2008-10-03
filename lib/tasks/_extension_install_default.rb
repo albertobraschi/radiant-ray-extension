8506480270f65a012ecb3d013f6cb732a94c18a1
johnmuhl <git@johnmuhl.com>
2008-10-02 21:43:08 -0600

    make this work for any kind of install

--- lib/tasks/_extension_install_default.rb
+++ lib/tasks/_extension_install_default.rb
@@ -6,9 +6,14 @@ if @hub
 else
   repository = master_repo
 end
-if File.exist?(".git/HEAD")
-  system "git submodule add #{repository}/radiant-#{github_name}-extension.git #{@path}/#{vendor_name}"
+if @fullname
+  extension = @fullname
 else
-  system "git clone #{repository}/radiant-#{github_name}-extension.git #{@path}/#{vendor_name}"
+  extension = "radiant-#{github_name}-extension"
+end
+if File.exist?(".git/HEAD")
+  system "git submodule add #{repository}/#{extension}.git #{@path}/#{vendor_name}"
+else
+  system "git clone #{repository}/#{extension}.git #{@path}/#{vendor_name}"
 end
 require "#{@task}/_extension_post_install.rb"
