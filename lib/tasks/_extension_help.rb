30c80db57145e25f293de34c7ac0d467f5b2e848
johnmuhl <git@johnmuhl.com>
2008-10-03 11:56:37 -0600

    show off the new hotness on the ray:help task

--- lib/tasks/_extension_help.rb
+++ lib/tasks/_extension_help.rb
@@ -1,19 +1,3 @@
-download_preference = File.new("vendor/extensions/ray/config/download.txt", "r")
-ray_download = download_preference.gets
-download_preference.close
-mkdir_p "vendor/extensions"
-if ray_download == "git\n"
-  git_check = File.new(".git/HEAD", "r") rescue nil
-  if git_check == nil
-    system "git clone git://github.com/saturnflyer/radiant-help-extension.git vendor/extensions/help"
-  else
-    system "git submodule add git://github.com/saturnflyer/radiant-help-extension.git vendor/extensions/help"
-    git_check.close
-  end
-else
-  mkdir_p "vendor/extensions/ray/tmp"
-  @ext_repo = "http://github.com/saturnflyer/"
-  @repo_name = "help"
-  require 'vendor/extensions/ray/lib/tasks/_extension_install_http_special.rb'
-end
-require 'vendor/extensions/ray/lib/tasks/_restart_server.rb'
+@name = "help"
+@hub  = "saturnflyer"
+require "#{@task}/_extension_install.rb"
