86462cef66463162d88d29bd16eb20df5e8f9115
johnmuhl <git@johnmuhl.com>
2008-10-02 19:00:33 -0600

    move restart into the post-install phase restructure a bit

--- lib/tasks/_extension_post_install.rb
+++ lib/tasks/_extension_post_install.rb
@@ -1,33 +1,46 @@
-name = ENV['name']
-vendor_name = name.gsub(/\-/, "_")
-task_check = File.new("vendor/extensions/#{vendor_name}/lib/tasks/#{vendor_name}_extension_tasks.rake", "r") rescue nil
-if task_check != nil
+vendor_name = @name.gsub(/\-/, "_")
+# determine what tasks need to be run
+begin
+  tasks = File.open("#{@path}/#{vendor_name}/lib/tasks/#{vendor_name}_extension_tasks.rake", "r")
   counter = 1
-  while (line = task_check.gets)
-    install_search = line.include? ":install"
+  # check for install task
+  while (line = tasks.gets)
+    install_task = line.include? ":install"
     break if install_search
     counter = counter + 1
   end
-  task_check.close
-  if install_search
+  tasks.close
+  # just run the install task if it's there
+  if install_task
     system "rake radiant:extensions:#{vendor_name}:install"
   else
-    task_check = File.new("vendor/extensions/#{vendor_name}/lib/tasks/#{vendor_name}_extension_tasks.rake", "r") rescue nil
-    while (line = task_check.gets)
-      migrate_search = line.include? ":migrate"
-      update_search = line.include? ":update"
-      if migrate_search
+    tasks = File.open("#{@path}/#{vendor_name}/lib/tasks/#{vendor_name}_extension_tasks.rake", "r")
+    counter = 1
+    # check for migrate and update tasks
+    while (line = tasks.gets)
+      migrate_task = line.include? ":migrate"
+      update_task = line.include? ":update"
+      # run the migrate task
+      if migrate_task
         system "rake radiant:extensions:#{vendor_name}:migrate"
       end
-      if update_search
+      # run the update task
+      if update_task
         system "rake radiant:extensions:#{vendor_name}:update"
       end
       counter = counter + 1
     end
-    task_check.close
+    tasks.close
   end
+  puts "=============================================================================="
+  puts "The #{vendor_name} extension has been installed."
+  puts "To disable it run: rake ray:dis name=#{vendor_name}"
+  puts "=============================================================================="
+rescue
+  puts "=============================================================================="
+  puts "I couldn't find any tasks to run for the #{vendor_name} extension."
+  puts "So it's hard to tell weather it installed correctly."
+  puts "Please manually verify the installation."
+  puts "=============================================================================="
 end
-puts "==="
-puts "The #{vendor_name} extension has been installed or updated."
-puts "To disable it later Use: rake ray:dis name=#{vendor_name}"
-puts "==="
+require "#{@task}/_restart_server.rb"
