86140fe4ad31861555cae276a9641a5c6f243f29
johnmuhl <git@johnmuhl.com>
2008-10-03 21:00:54 -0600

    restructure

--- lib/tasks/_radiant_edge.rb
+++ lib/tasks/_radiant_edge.rb
@@ -1,44 +1,55 @@
-download_preference = File.new("vendor/extensions/ray/config/download.txt", "r")
-ray_download = download_preference.gets
-download_preference.close
-if ray_download == "git\n"
-  submodule_check = File.new(".gitmodules", "r") rescue nil
-  if submodule_check
-    counter = 1
-    while (line = submodule_check.gets)
-      radiant_search = line.include? "\[submodule\ \"vendor\/radiant\"\]"
-      break if radiant_search
-      counter = counter + 1
-    end
-    submodule_check.close
-    if radiant_search
+if File.exist?("#{@conf}/download.txt")
+  download_conf = File.open("#{@conf}/download.txt", "r")
+  download_pref = download_conf.gets
+  download_conf.close
+  if download_pref == "git\n"
+    puts "=============================================================================="
+    begin
+      Dir.open("vendor/radiant")
       system "cd vendor/radiant; git remote update"
-    else
-      system "git submodule add git://github.com/radiant/radiant.git vendor/radiant"
+    rescue
+      if File.exist?(".git/HEAD")
+        system "git submodule add git://github.com/radiant/radiant.git vendor/radiant"
+      else
+        system "git clone git://github.com/radiant/radiant.git vendor/radiant"
+      end
     end
-    puts "==="
     puts "Radiant has been updated to the latest edge version."
-    puts "==="
+    puts "=============================================================================="
+  elsif download_pref == "http\n"
+    puts "=============================================================================="
+    system "mkdir -p #{@ray}/tmp"
+    if File.exist?("vendor/radiant/LICENSE")
+      system "rm -r vendor/radiant"
+    end
+    github_url = URI.parse("http://github.com/radiant/radiant/tarball/master")
+    found = false
+    until found
+      host, port = github_url.host, github_url.port if github_url.host && github_url.port
+      github_request = Net::HTTP::Get.new(github_url.path)
+      github_response = Net::HTTP.start(host, port) {|http|  http.request(github_request) }
+      github_response.header['location'] ? github_url = URI.parse(github_response.header['location']) :
+    found = true
+    end
+    open("#{@ray}/tmp/radiant.tar.gz", "wb") { |file|
+      file.write(github_response.body)
+    }
+    system "cd #{@ray}/tmp; tar xzvf radiant.tar.gz; rm *.tar.gz"
+    system "mv #{@ray}/tmp/* vendor/radiant"
+    system "rm -r #{@ray}/tmp"
+    puts "Radiant has been updated to the latest edge version."
+    puts "=============================================================================="
+  else
+    puts "=============================================================================="
+    puts "Your download preference is broken."
+    puts "Please run: rake ray:git"
+    puts "=============================================================================="
   end
-elsif ray_download == "http\n"
-  mkdir_p "vendor/extensions/ray/tmp"
-  rm_r "vendor/radiant"
-  github_url = URI.parse("http://github.com/radiant/radiant/tarball/master")
-  found = false
-  until found
-    host, port = github_url.host, github_url.port if github_url.host && github_url.port
-    github_request = Net::HTTP::Get.new(github_url.path)
-    github_response = Net::HTTP.start(host, port) {|http|  http.request(github_request) }
-    github_response.header['location'] ? github_url = URI.parse(github_response.header['location']) :
-  found = true
-  end
-  open("vendor/extensions/ray/tmp/radiant.tar.gz", "wb") { |file|
-    file.write(github_response.body)
-  }
-  system "cd vendor/extensions/ray/tmp; tar xzvf radiant.tar.gz; rm *.tar.gz"
-  system "mv vendor/extensions/ray/tmp/* vendor/radiant"
-  rm_rf "vendor/extensions/ray/tmp"
-  puts "==="
-  puts "Radiant has been updated to the latest edge version."
-  puts "==="
+else
+  puts "=============================================================================="
+  puts "Looks like you haven't setup your preferred download method."
+  puts "Let's get that setup now..."
+  require "#{@task}/_setup_download_preference.rb"
+  puts "With that out of the way try that command again."
+  puts "=============================================================================="
 end
