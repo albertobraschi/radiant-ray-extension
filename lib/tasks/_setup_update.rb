eaf90a36924e699be1c924a60bc042d2008c232d
johnmuhl <git@johnmuhl.com>
2008-10-03 21:12:42 -0600

    restructure

--- lib/tasks/_setup_update.rb
+++ lib/tasks/_setup_update.rb
@@ -1,20 +1,9 @@
-legacy_check = File.new("config/ray.setup", "r") rescue nil
-if legacy_check
-  mkdir_p "vendor/extensions/ray/config"
-  system "mv config/ray.setup vendor/extensions/ray/config/download.txt"
-  puts "==="
-  puts "Updated to Ray 1.1"
-  puts "==="
-else
-  upgrade = File.new("vendor/extensions/ray/config/download.txt", "r") rescue nil
-  if upgrade
-    puts "==="
-    puts "Already up to date."
-    puts "==="
-  else
-    legacy_check = File.new("vendor/extensions/ray/config/setup.txt", "r") rescue nil
-    if legacy_check
-      system "mv vendor/extensions/ray/config/setup.txt vendor/extensions/ray/config/download.txt"
-    end
-  end
+if File.exist?("config/ray.setup")
+  system "mv config/ray.setup #{@conf}/download.txt"
 end
+if File.exist?("#{@conf}/setup.txt")
+  system "mv #{@conf}/setup.txt #{@conf}/download.txt"
+end
+puts "=============================================================================="
+puts "Ray is up to date."
+puts "=============================================================================="
